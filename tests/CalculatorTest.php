60d2acfb793e76f16e9ca74572e19fdafce8243e
Romualdo Dasig <dasig.rg@a5project.com>
2019-04-17 14:08:58 +0800

    Fix test.

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -7,6 +7,6 @@ class CalculatorTest extends PHPUnit_Framework_TestCase {
   public function testAdd() {
     $calculator = new Calculator();
     $result = $calculator->add(2, 3);
-    $this->assertEquals(6, $result);
+    $this->assertEquals(5, $result);
   }
 }
